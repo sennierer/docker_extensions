c35fb02558ab322b1ed5887f47b90f55c75e14ca
Matthias Schlögl <m.schloegl@gmail.com>
2018-07-11 21:15:37 +0200

    typo in start scriptwq

--- http_wsgi3_celery/start_celery.sh
+++ http_wsgi3_celery/start_celery.sh
@@ -2,5 +2,5 @@
 if [ -d "/var/www/html/myenv" ]; then
   source /var/www/html/myenv && service celeryd start
 else
-  su - user -c "virtualenv -p python3 /var/www/html/myenv" && su - user -c  "source /var/www/html/myenv/bin/activate" && su - user -c  "pip install -r /var/www/html/requirements.txt" && service celery start
+  su - user -c "virtualenv -p python3 /var/www/html/myenv" && su - user -c  "source /var/www/html/myenv/bin/activate" && su - user -c  "pip install -r /var/www/html/requirements.txt" && service celeryd start
 fi

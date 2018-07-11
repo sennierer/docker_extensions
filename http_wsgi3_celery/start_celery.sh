3cd46e2dddc3fa7a33adf95c6d46585f81463a02
Matthias Schlögl <m.schloegl@gmail.com>
2018-07-11 20:57:21 +0200

    add the bash start script

---
+++ http_wsgi3_celery/start_celery.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ -d "/var/www/html/myenv" ]; then
+  source /var/www/html/myenv && service celeryd start
+else
+  virtualenv -p python3 /var/www/html/myenv && source /var/www/html/myenv/bin/activate && pip install -r /var/www/html/requirements.txt && service celery start
+fi

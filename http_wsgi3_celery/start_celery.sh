c57d3687c8ddddadb422d9159c6de369f3b057e6
Matthias Schlögl <m.schloegl@gmail.com>
2018-07-11 21:38:25 +0200

    change start script for auto venv

--- http_wsgi3_celery/start_celery.sh
+++ http_wsgi3_celery/start_celery.sh
@@ -2,5 +2,5 @@
 if [ -d "/var/www/html/myenv" ]; then
   source /var/www/html/myenv && service celeryd start
 else
-  su - user -c "virtualenv -p python3 /var/www/html/myenv && source /var/www/html/myenv/bin/activate && pip install -r /var/www/html/requirements.txt" && service celeryd start
+  su - user -c "virtualenv -p python3 /var/www/html/myenv && . /var/www/html/myenv/bin/activate && pip install -r /var/www/html/requirements.txt" && service celeryd start
 fi

e528d42de493a5cacc15277d0251591e5e731c4f
Matthias Schlögl <m.schloegl@gmail.com>
2018-07-11 21:03:23 +0200

    use su to use user instead of root

--- http_wsgi3_celery/start_celery.sh
+++ http_wsgi3_celery/start_celery.sh
@@ -2,5 +2,5 @@
 if [ -d "/var/www/html/myenv" ]; then
   source /var/www/html/myenv && service celeryd start
 else
-  virtualenv -p python3 /var/www/html/myenv && source /var/www/html/myenv/bin/activate && pip install -r /var/www/html/requirements.txt && service celery start
+  su - user -c "virtualenv -p python3 /var/www/html/myenv" && su - user -c  "source /var/www/html/myenv/bin/activate" && su - user -c  "pip install -r /var/www/html/requirements.txt" && service celery start
 fi

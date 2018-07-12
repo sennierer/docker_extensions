74a06db31a8db622cb725b68ebdae33002caf6fc
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2018-07-12 14:33:31 +0200

    working version

--- http_wsgi3_celery/start_celery.sh
+++ http_wsgi3_celery/start_celery.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ -d "/var/www/html/myenv" ]; then
-  source /var/www/html/myenv && service celeryd start
+  source /var/www/html/myenv/bin/activate && service celeryd start
 else
-  su - user -c "virtualenv -p python3 /var/www/html/myenv && . /var/www/html/myenv/bin/activate && pip install -r /var/www/html/requirements.txt" && service celeryd start
+  su - user -c "virtualenv -p python3 /var/www/html/myenv && . /var/www/html/myenv/bin/activate && pip install -r /var/www/html/requirements.txt"
+  source /var/www/html/myenv && service celeryd start
 fi
